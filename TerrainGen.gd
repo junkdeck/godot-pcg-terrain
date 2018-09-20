809773408bebdcb5c07ba8ee44082463579d9eca
junkdeck <fred@mslsr.com>
2018-09-20 22:27:02 +0200

    create polygon collision shape for terrain gen
    rename Node2D.gd to TerrainGen.gd

--- TerrainGen.gd
+++ TerrainGen.gd
@@ -4,8 +4,8 @@ var update_interval = 1
 var update_timer = update_interval
 
 func _draw():
-	var terrain_res = 200
-	var terrain_step = 10
+	var terrain_res = 100
+	var terrain_step = 5
 	var terrain_variation = 5.0
 	var baseline = Vector2(0.0, 250.0)
 	var points = PoolVector2Array()
@@ -13,27 +13,31 @@ func _draw():
 	var col = CollisionShape2D.new()
 	var poly = CollisionPolygon2D.new()
 	
-	points.push_back(Vector2(0.0,0.0))
+	points.push_back(baseline)
 	
 	for i in range(terrain_res+1):
 		randomize()
-		
-		var terrain_jump = 10.0 if randf()*100.0+1.0 > 95 else 1
+		var terrain_jump = 10.0 if randf() * 100.0 + 1.0 > 95 else 1
 		
 		var last_point = points[i]
 		var point = Vector2(i * (terrain_step), last_point.y + (randf()*2-1) * (terrain_variation * terrain_jump))
+		
 		points.push_back(point)
 	
 	for point in range(terrain_res):
-		draw_line(baseline + points[point], baseline + points[point+1], Color(1.0,0.0,0.0))
+		draw_line(points[point], points[point+1], Color(1.0,0.0,0.0))
+	
+	# close bounding polygon
+	points.append_array([points[points.size()-1] + Vector2(0.0, 1000.0), Vector2(points[0].x, 1000)])
 	
 	poly.polygon = points
 	col.shape = poly
+	
+	for child in get_children():
+		child.queue_free()
+		
 	self.add_child(poly)
-	
-	print(self.get_children())
-	
-	
+
 
 func _process(delta):
 	if(update_timer <= 0):
